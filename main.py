7a880f21179dec45c38c68e89a475d3e39d74742
joe <joseph.edwards@itv.com>
2021-02-17 13:45:47 +0000

    testicals

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('test')
+from kivy.config import Config
+Config.set('graphics', 'width', '700')
+Config.set('graphics', 'height', '1000')
+from kivymd.app import MDApp
+
+class TestApp(MDApp):
+    pass
+
+
+TestApp().run()
